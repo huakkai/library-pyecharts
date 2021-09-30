6c23e372979cf2bcc511cdb42a11e50b58d9ec0f
esplets <>
2021-09-30 15:18:12 +0800

    library pyecharts

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Library pyecharts',
+    'version': '12.0.0.1',
+    'description': '',
+    'author': 'huaqiangyan@163.com',
+    'depends': ['web'],
+    'data': [
+        'views/assets_backend.xml',
+        'views/home.xml',
+    ],
+    'qweb': [
+        'static/src/xml/*.xml',
+    ],
+}
