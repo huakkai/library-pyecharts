053a62fa67c573aae6f33b0d09bc502ef915ed9b
esplets <>
2021-09-30 20:12:02 +0800

    扩充例子

--- controllers/echarts.py
+++ controllers/echarts.py
@@ -3,7 +3,9 @@ import json
 import random
 from pyecharts.charts import Bar
 from pyecharts import options as opts
-from pyecharts.globals import ThemeType
+from pyecharts.charts import Line
+from pyecharts.charts import Pie
+from pyecharts.faker import Faker
 
 import logging
 
@@ -35,11 +37,10 @@ def bar1_base():
 
 def bar3_base():
     c = (
-        Bar(init_opts=opts.InitOpts(theme=ThemeType.WALDEN))
-            .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
-            .add_yaxis("商家A", [5, 20, 36, 10, 75, 90])
-            .add_yaxis("商家B", [15, 6, 45, 20, 35, 66])
-            .set_global_opts(title_opts=opts.TitleOpts(title="主标题", subtitle="副标题"))
+        Pie()
+            .add("", [list(z) for z in zip(Faker.choose(), Faker.values())])
+            .set_global_opts(title_opts=opts.TitleOpts(title="Pie-基本示例"))
+            .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
             .dump_options_with_quotes()
     )
     return c
@@ -56,12 +57,28 @@ def bar2_base():
 
 
 def bar4_base():
+    x_data = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    y_data = [820, 932, 901, 934, 1290, 1330, 1320]
+
     c = (
-        Bar(init_opts=opts.InitOpts(theme=ThemeType.LIGHT))
-            .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
-            .add_yaxis("商家A", [5, 20, 36, 10, 75, 90])
-            .add_yaxis("商家B", [15, 6, 45, 20, 35, 66])
-            .set_global_opts(title_opts=opts.TitleOpts(title="主标题", subtitle="副标题"))
+        Line()
+            .set_global_opts(
+            tooltip_opts=opts.TooltipOpts(is_show=False),
+            xaxis_opts=opts.AxisOpts(type_="category"),
+            yaxis_opts=opts.AxisOpts(
+                type_="value",
+                axistick_opts=opts.AxisTickOpts(is_show=True),
+                splitline_opts=opts.SplitLineOpts(is_show=True),
+            ),
+        )
+            .add_xaxis(xaxis_data=x_data)
+            .add_yaxis(
+            series_name="",
+            y_axis=y_data,
+            symbol="emptyCircle",
+            is_symbol_show=True,
+            label_opts=opts.LabelOpts(is_show=False),
+        )
             .dump_options_with_quotes()
     )
     return c
