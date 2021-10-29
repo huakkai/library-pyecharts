50e314f4aa29320acb4c4e4fe2547c7f98bbe939
esplets <>
2021-10-29 10:24:45 +0800

    13.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Library pyecharts',
-    'version': '12.0.0.1',
+    'version': '13.0.0.1',
     'description': '',
     'author': 'huaqiangyan@163.com',
     'depends': ['web'],
@@ -9,26 +9,11 @@
         'security/ir.model.access.csv',
         'data/echarts_type_data.xml',
         'data/dashboard_data.xml',
-        # 'views/assets_backend.xml',
+        'views/assets_backend.xml',
         'views/home.xml',
         'views/echarts_type_view.xml',
         'views/dashboard.xml',
     ],
-    'assets': {
-        'web.assets_backend': [
-            'library_pyecharts/static/src/js/echarts.min.js',
-            'library_pyecharts/static/src/js/china.js',
-            'library_pyecharts/static/src/js/load_echarts_bar.js',
-            'library_pyecharts/static/src/css/adminlte.min.css',
-            'library_pyecharts/static/src/js/theme_dark.js',
-            'library_pyecharts/static/src/js/theme_macarons.js',
-            'library_pyecharts/static/src/js/theme_vintage.js',
-            'library_pyecharts/static/src/js/theme_infographic.js',
-            'library_pyecharts/static/src/js/theme_roma.js',
-            'library_pyecharts/static/src/js/theme_shine.js',
-        ],
-        'web.assets_qweb': [
-            'library_pyecharts/static/src/xml/**/*',
-        ],
-    },
+    'qweb': ['static/src/xml/*.xml'],
+    'js': ['static/src/js/*.js'],
 }
